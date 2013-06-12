71596f6b3f24297fda221b78c7b90acc59ef8e65
Lior Kesos <lior@linnovate.net>
2013-06-12 21:59:12 +0000

    added high and low button tracking

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -4,7 +4,12 @@ function IndexController($scope, Global, Leads){
 	$scope.openModal = function(modalID, event) {
 		Global.modal.path = "views/dialogs/" + modalID + ".html";
 		Global.modal.class = "open";
-
+		if (event.pageY > 500){
+		  $("html, body").animate({ scrollTop: 0 }, "slow");
+			if (_gaq) _gaq.push(['_trackEvent', 'meanlean', 'submit-button', 'low', 1]);
+		}else{
+			if (_gaq) _gaq.push(['_trackEvent', 'meanlean', 'submit-button', 'high', 1]);
+		}
 		event.stopPropagation();
 	};
 
